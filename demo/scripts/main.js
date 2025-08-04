178293e55e68d94e964a6f55b1a5cbdab3317ecc
Tomas Opazo <toopazo@proton.me>
2025-08-04 16:04:47 -0400

    Mejora en claridad de comandos

--- demo/scripts/main.js
+++ demo/scripts/main.js
@@ -1,6 +1,13 @@
 const listItems = document.querySelectorAll("li");
+const listItems2 = document.getElementsByClassName("old-websites")[0].childNodes[1].querySelectorAll("li");
+
+console.log(document.getElementsByClassName("big-ul"));
+console.log(document.getElementsByClassName("old-websites"));
 
 function toggleDone(e) {
+  // console.log(e);
+  // console.log(e.target.className);
+  
   if (!e.target.className) {
     e.target.className = "done";
   } else {
@@ -8,6 +15,8 @@ function toggleDone(e) {
   }
 }
 
-listItems.forEach((item) => {
+console.log("listItems2[0].innerHTML: " + listItems2[0].innerHTML);
+
+listItems2.forEach((item) => {
   item.addEventListener("click", toggleDone);
 });
